b81bcec5a97504bb3a369947993123bb51701f40
mumumuyi <2842935211@qq.com>
2018-07-19 15:38:04 +0800

    商品销售静态页面

--- src/views/commodity-sales.vue
+++ src/views/commodity-sales.vue
@@ -24,31 +24,126 @@
          </div>
          <div class=" sale-commodity-box panel">
           <ul class="clearfix">
-            <li class="commodity-box active"><span>饮料</span></li>
-            <li class="commodity-box"><span>零食</span></li>
-            <li class="commodity-box"><span>点卡</span></li>
-            <li class="commodity-box"><span>超市</span></li>
-            <li class="commodity-box"><span>休闲小食</span></li>
-            <li class="commodity-box"><span>果汁</span></li>
-            <li class="commodity-box"><span>外设</span></li>
-            <li class="commodity-box"><span>便当</span></li>
-            <li class="commodity-box"><span>咖啡</span></li>
-            <li class="commodity-box"><span>饮料</span></li>
-            <li class="commodity-box"><span>饮料</span></li>
-            <li class="commodity-box"><span>饮料</span></li>
+            <li class="commodity-box active"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
+            <li class="commodity-box"><p class="name">商品商品商品</p><span  class="value">￥</span></li>
           </ul>
+
          </div>
-         <div class="sale-Stateselection-box panel">
-           <p>*请选择状态</p>
+         <div class="sale-State-box panel">
+           <p><span class="sign">*</span>请选择状态:</p>
           <ul class="clearfix">
-            <li class="active-box active"><span>常温</span></li>
-            <li class="active-box"><span>加热</span></li>
-            <li class="active-box"><span>加冰</span></li>
+            <li class="State-box active"><span>常温</span></li>
+            <li class="State-box"><span>加热</span></li>
+            <li class="State-box"><span>加冰</span></li>
           </ul>
          </div>
       </div>
       <div class="bar-right-container">
+        <div class="shopping-list panel">
+              <ul class="clearfix">
+                <li>
+                  <div class="list">
+                    <div class="list-shop">商品1</div>
+                    <div class="list-number">1*少冰</div>
+                    <div class="list-value"> 4.00</div>
+                    <div class="list-button"> <button class="btn">-</button></div>
+                  </div> </li>
+              </ul>
+          <ul class="clearfix">
+            <li>
+              <div class="list">
+                <span class="list-shop">商品1</span>
+                <span class="list-number">1*少冰</span>
+                <span class="list-value"> 4.00</span>
+                <span class="list-button"> <button class="btn">-</button></span>
+              </div> </li>
+          </ul>
+          <ul class="clearfix">
+            <li>
+              <div class="list">
+                <span class="list-shop">商品1</span>
+                <span class="list-number">1*少冰</span>
+                <span class="list-value"> 4.00</span>
+                <span class="list-button"> <button class="btn">-</button></span>
+              </div> </li>
+          </ul>
+          <ul class="clearfix">
+            <li>
+              <div class="list">
+                <span class="list-shop">商品1</span>
+                <span class="list-number">1*少冰</span>
+                <span class="list-value"> 4.00</span>
+                <span class="list-button"> <button class="btn">-</button></span>
+              </div> </li>
+          </ul>
 
+            <div class="button">
+              <button class="btn btn-page ">上一页</button>
+              <button class="btn btn-page ">下一页</button>
+            </div>
+          <div class="operation">
+            <input type="button" value="-" onclick="jian()">
+          <input class="zhi" type="text" value="0">
+            <input type="button" value="+" onclick="jia()">
+          </div>
+        </div>
+
+        <div class="recharge-method-box panel">
+          <ul @click="selectPayment($event)">
+            <li class="method-type" data-payment="现金"><i class="iconfont icon-xianjin"></i>
+              <p>现金</p>
+            </li>
+            <li class="method-type" data-payment="支付宝"><i class="iconfont icon-zhifubao"></i>
+              <p>支付宝</p>
+            </li>
+            <li class="method-type" data-payment="微信"><i class="iconfont icon-weixin"></i>
+              <p>微信</p>
+            </li>
+            <li class="method-type" data-payment="POS机"><i class="iconfont icon-shouyinji"></i>
+              <p>POS机</p>
+            </li>
+            <li class="method-type" data-payment="维护金"><i class="iconfont icon-weihujin"></i>
+              <p>维护金</p>
+            </li>
+          </ul>
+        </div>
+        <div class="accounts panel">
+          <ul class="clearfix">
+            <li>数量：<span>6</span></li>
+          <li>合计：<span class="value">￥300.00</span></li>
+          <li><button class="btn btn-empty">清空</button></li>
+          <li><button class="btn btn-account">结算</button></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+      <div class="bar-bottom-container  panel">
+        <div class="bottom-package-box">
+        <div class="package">优惠套餐</div>
+          <div class=" btn button-left"> <button>《gdfn</button></div>
+          <div class="owl-carousel owl-theme ">
+        <ul class="clearfix">
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+          <li class="package-box"><p class="name">商品商品商品</p><span class="value">￥</span></li>
+        </ul>
+          </div>
+          <div class="btn button-right"><button>》sav</button></div>
       </div>
     </div>
   </div>
