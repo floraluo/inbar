0b06abff4cd63041db65f96164c0f35ea3a94a18
mumumuyi <2842935211@qq.com>
2018-09-10 18:35:00 +0800

    个人基本信息

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,15 @@
 <template>
-  <h2>页面找不到了！</h2>
+  <div class="page-main">
+    <div class="panel-center ">
+      <div class="left-p">
+        <img src="../assets/img/404.png">
+      </div>
+      <div class="right-w ">
+        <h1>很抱歉！您访问的页面不存在...</h1>
+        <button class="btn btn-primary">返回首页<a href="#"></a></button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +17,9 @@
     name: '404'
   }
 </script>
+<style lang="scss">
+ .panel-center{
+   position: relative;
+ }
+
+</style>
