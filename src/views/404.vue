9acd8eadec0b26834b1f0319e3d01daf4d76a0e0
mumumuyi <2842935211@qq.com>
2018-09-11 16:28:37 +0800

    报错页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="page-main">
     <div class="panel-center ">
-      <div class="left-p">
+      <div class="col-xs-6 text-right">
         <img src="../assets/img/404.png">
       </div>
-      <div class="right-w ">
-        <h1>很抱歉！您访问的页面不存在...</h1>
-        <button class="btn btn-primary">返回首页<a href="#"></a></button>
-      </div>
+        <div class="col-xs-6">
+          <h1>很抱歉！您访问的页面不存在...</h1>
+          <button class="btn btn-primary">返回首页<a href="#"></a></button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -17,9 +15,12 @@
     name: '404'
   }
 </script>
-<style lang="scss">
- .panel-center{
-   position: relative;
- }
+<style lang="scss" scoped>
+  @import "../sass/variables";
+.panel-center{
+  height: calc(100vh - #{$nav-height} - #{$tab-height} - #{$footer-height});
+  display: flex;
+  align-items: center;
+}
 
 </style>
---
+++ src/views/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="panel-center ">
+    <div class="col-xs-6 text-right">
+      <img src="../assets/img/500.png">
+    </div>
+    <div class="col-xs-6">
+      <h1>出错啦！找不到您请求的页面...</h1>
+      <button class="btn btn-primary">返回首页<a href="#"></a></button>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "error"
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import "../sass/variables";
+  .panel-center{
+    height: calc(100vh - #{$nav-height} - #{$tab-height} - #{$footer-height});
+    display: flex;
+    align-items: center;
+  }
+
+</style>
